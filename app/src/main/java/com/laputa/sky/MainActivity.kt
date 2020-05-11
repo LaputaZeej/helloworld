38c6346d8580f7608fb3d5d040cd0a9347cf7490
xiang <xiangpl@hadlinks.com>
2020-05-11 11:00:34 +0800

    master更新 hello()方法

--- app/src/main/java/com/laputa/sky/MainActivity.kt
+++ app/src/main/java/com/laputa/sky/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.laputa.sky
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
@@ -11,5 +12,9 @@ class MainActivity : AppCompatActivity() {
 
         // 这段说明 代表tag：v1.0.0标签下的一次bug修复 测试冲突02
         // 这段说明 代表tag：v1.0.0标签下的一次bug修复 02
+    }
+
+    fun hello() {
+        startActivity(Intent(this, HelloActivity::class.java))
     }
 }
