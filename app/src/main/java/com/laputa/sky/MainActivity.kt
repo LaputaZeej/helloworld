c7795219712c9fb8e596e6d2b7ff9617c65f0b56
xiang <xiangpl@hadlinks.com>
2020-05-11 14:53:54 +0800

    开发新功能feature 02

--- app/src/main/java/com/laputa/sky/MainActivity.kt
+++ app/src/main/java/com/laputa/sky/MainActivity.kt
@@ -9,6 +9,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        // hotfix 线上修复（测试冲突 hotfix vs feature02）
 
         // 这段说明 代表tag：v1.0.0标签下的一次bug修复 测试冲突02
         // 这段说明 代表tag：v1.0.0标签下的一次bug修复 02
