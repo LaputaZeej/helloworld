f44b13f6436dec14da75f745c1ac5231702177ce
xiang <xiangpl@hadlinks.com>
2020-05-11 16:48:18 +0800

    git分支管理说明

--- app/src/main/java/com/laputa/sky/MainActivity.kt
+++ app/src/main/java/com/laputa/sky/MainActivity.kt
@@ -4,6 +4,28 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
+/**
+ * git代码管理
+ *
+ *  《分支》                                《周期》                    《说明》
+ * 【master】                              常驻分支                   主分支，有且只有一个。提供给用户使用的版本，都在这个分支上发布。需tag。
+ * 【dev】                                 常驻分支                   日常开发分支，测试通过，由主分支合并dev对外发布。
+ *  hotFixs/bugFixs                        补丁分支                   正式发布后，出现bug，创建的bug修复分支。从master分出来的，修复后合并到master和dev，如果存在release，则同样合并到。
+ *  releases                               预发分支                   发布正式版本之前，即合并到master之前，预发布的版本进行测试的分支。测试结束后，合并到dev和master分支，并删除。
+ *  features                               功能分支                   临时分支，从dev分支分出来的，开发完又dev合并，然后删除。
+ *
+ *  ..................................................................................................................................................................
+ *
+ * 产品分支（特殊情况）
+ * 【product】                             常驻分支                   特殊的项目，独立于master分支，不会合并到master，区别迭代。
+ * dev-product
+ * features-product
+ * releases-product
+ * hotFixs-product/bugFixs-prodcut
+ *
+ * product分支，如果有相同业务功能的，可以从master合并进product？
+ *
+ */
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
